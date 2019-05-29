1c516f9791c8180e0a15ab9724ff382436f27b89
Mahadi <mahadihasan@interconnectionbd.com>
2019-05-29 12:43:01 +0600

    initial routing added

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const About = () =>(
+    <div>
+        <Navbar />
+        
+      <h1>this is bitzprice About Section</h1>
+      <p>lorem ipsum lorem ipsum lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum</p>
+  </div>
+)
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Link from 'next/link';
+import Navbar from "../components/Navbar"
+
+const Index = () =>(
+  <div>
+      <Navbar />
+      <h1>Welcome to bitzprice Application!!</h1>
+  </div>
+)
+
+export default Index;
